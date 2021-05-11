3dc7093608b0b326b66f777d23f071da22d912a6
CHA-g9 <80168389+CHA-g9@users.noreply.github.com>
2021-05-11 15:18:17 +0900

    Update settings.py

--- 5주차과제/firstproject/firstproject/settings.py
+++ 5주차과제/firstproject/firstproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7)29f*t27amc3(22(01vik#)9a#997gehp112ufox5je739a@z'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
